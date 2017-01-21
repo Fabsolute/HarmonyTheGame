db9d2c32b1202f0b876c84e82bceb04292201bb6
NecipAllef <necipallef@gmail.com>
2017-01-21 11:25:42 +0300

    Ahmo noob

--- Assets/Scripts/Scream.cs
+++ Assets/Scripts/Scream.cs
@@ -1,7 +1,19 @@
 ﻿
 using UnityEngine;
+using System.Collections.Generic;
 
-public class Scream : MonoBehaviour {
+public class Scream : MonoBehaviour 
+{
+	public enum Direction
+	{
+		RIGHT,
+		TOP,
+		LEFT,
+		BOTTOM,
+		UNKNOWN
+	};
+
+	private const float MIN_DIST = 0.01f;
 
 	public Screamer Screamer;
 
@@ -12,6 +24,8 @@ public class Scream : MonoBehaviour {
 	private float collisionTopStart;
 	private Material screamMaterial;
 	private PolygonCollider2D polygonCollider;
+	private List<Vector3> obstaclePoints = new List<Vector3>();
+	private Direction screamDirection = Direction.UNKNOWN;
 	void Start () {
 		
 		var screamSpriteRenderer =GetComponent<SpriteRenderer>();
@@ -19,15 +33,38 @@ public class Scream : MonoBehaviour {
 			screamMaterial = screamSpriteRenderer.material;
 		}
 		polygonCollider = GetComponent<PolygonCollider2D>();
-	}
-	
-	void Update () {
-		if(screamMaterial!=null){
-			Debug.Log("ok");
+
+		if (Mathf.Abs(polygonCollider.bounds.center.x - transform.position.x) > MIN_DIST)
+		{
+			if (polygonCollider.bounds.center.x < transform.position.x)
+			{
+				screamDirection = Direction.LEFT;
+			} else 
+			{
+				screamDirection = Direction.RIGHT;
+			}
+		} else if (Mathf.Abs(polygonCollider.bounds.center.y - transform.position.y) > MIN_DIST)
+		{
+			if (polygonCollider.bounds.center.y < transform.position.y)
+			{
+				screamDirection = Direction.BOTTOM;
+			} else 
+			{
+				screamDirection = Direction.TOP;
+			}	
+		} else 
+		{
+			screamDirection = Direction.UNKNOWN;
+			Debug.LogError("Unknown sceam direction " + gameObject.name);
 		}
 	}
+	
+	void Update () 
+	{
 
-	void OnCollisionStay(Collision collision)
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
 	{
 		if(collision.collider.tag == "Box")
 		{
@@ -41,9 +78,80 @@ public class Scream : MonoBehaviour {
 				}
 			}
 
-			 var length = polygonCollider.bounds.extents * 2;
-			 var min= polygonCollider.bounds.min;
-			 var max = polygonCollider.bounds.max;
+			if (obstaclePoints.IndexOf(nearest_point) == -1)
+			{
+				// (new GameObject()).transform.position = nearest_point;
+				obstaclePoints.Add(nearest_point);
+
+				var center = polygonCollider.bounds.center;
+				var minPoint = polygonCollider.bounds.min;
+				var maxPoint = polygonCollider.bounds.max;
+				switch (screamDirection)
+				{
+					case Direction.LEFT:
+						{
+							if (nearest_point.y < center.y) // Obstacle is below 
+							{
+								var cutOutValueY = 1 - (center.y - nearest_point.y) / (center.y - minPoint.y);
+								screamMaterial.SetFloat("_CutOutBottom", cutOutValueY);
+								var cutOutValueX = (maxPoint.x - nearest_point.x) / (maxPoint.x - minPoint.x);
+								screamMaterial.SetFloat("_CutOutBottomStart", cutOutValueX);
+							} else // Obstacle is above
+							{
+								var cutOutValueY = (maxPoint.y - nearest_point.y) / (maxPoint.y - center.y);
+								screamMaterial.SetFloat("_CutOutTop", cutOutValueY);
+								var cutOutValueX = (maxPoint.x - nearest_point.x) / (maxPoint.x - minPoint.x);
+								screamMaterial.SetFloat("_CutOutTopStart", cutOutValueX);
+							}
+						}
+						break;
+					case Direction.RIGHT:
+						{
+							if (nearest_point.y < center.y) // Obstacle is below 
+							{
+								var cutOutValueY = 1 - (center.y - nearest_point.y) / (center.y - minPoint.y);
+								screamMaterial.SetFloat("_CutOutBottom", cutOutValueY);
+								var cutOutValueX = (nearest_point.x - minPoint.x) / (maxPoint.x - minPoint.x);
+								screamMaterial.SetFloat("_CutOutBottomStart", cutOutValueX);
+							} else // Obstacle is above
+							{
+								var cutOutValueY = (maxPoint.y - nearest_point.y) / (maxPoint.y - center.y);
+								screamMaterial.SetFloat("_CutOutTop", cutOutValueY);
+								var cutOutValueX = (nearest_point.x - minPoint.x) / (maxPoint.x - minPoint.x);
+								screamMaterial.SetFloat("_CutOutTopStart", cutOutValueX);
+							}
+						}
+						break;
+					default:
+					{
+						if (nearest_point.y < center.y) // Obstacle is below 
+						{
+							var cutOutValueY = 1 - (center.y - nearest_point.y) / (center.y - minPoint.y);
+							screamMaterial.SetFloat("_CutOutBottom", cutOutValueY);
+							if (nearest_point.x < center.x) // Obstacle is left
+							{
+								var cutOutValueX = 1 - (maxPoint.x - nearest_point.x) / (maxPoint.x - minPoint.x);
+							} 
+						} else // Obstacle is above
+						{
+							var cutOutValueY = 1 - (maxPoint.y - nearest_point.y) / (maxPoint.y - center.y);
+							var cutOutValueX = 1 - (maxPoint.x - nearest_point.x) / (maxPoint.x - minPoint.x);
+							screamMaterial.SetFloat("_CutOutTop", cutOutValueY);
+						}
+					}
+						break;
+				}
+			}
 		}
+	}
+
+	public void OnPositionChanged()
+	{
+		obstaclePoints.Clear();
+	}
+
+	public void SetCutOut(float newValue)
+	{
+		screamMaterial.SetFloat("_CutOut", newValue);
 	}
 }
